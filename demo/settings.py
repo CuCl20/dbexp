d3d10cc634bfc1a6bc44dc318959852390e30c6c
unknown <2403683410@qq.com>
2022-11-18 20:37:30 +0800

    db

--- demo/settings.py
+++ demo/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition 在此声明自建的子应用
 
 INSTALLED_APPS = [
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',#主应用
+    'main',  # 主应用
 ]
 
 MIDDLEWARE = [
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'html')],  #前端文件位置
+        'DIRS': [os.path.join(BASE_DIR, 'html')],  # 前端文件位置
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,21 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'demo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {#连接的mysql配置
+DATABASES = {  # 连接的mysql配置
     'default': {
-    'ENGINE': 'django.db.backends.mysql',#数据库的类型
-    'NAME': 'datademo', #所使用的的数据库的名字
-    'USER': 'root', #数据库服务器的用户
-    'PASSWORD': 'yhv5tgh233', #密码
-    'HOST': '127.0.0.1', #主机
-    'PORT': '3306', #端口
+        'ENGINE': 'django.db.backends.mysql',  # 数据库的类型
+        'NAME': 'datademo',  # 所使用的的数据库的名字
+        'USER': 'root',  # 数据库服务器的用户
+        'PASSWORD': 'yhv5tgh233',  # 密码
+        'HOST': '127.0.0.1',  # 主机
+        'PORT': '3306',  # 端口
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -118,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- main/funcs.py
+++ main/funcs.py
@@ -2,12 +2,13 @@ import pymysql
 import datetime
 
 
-def add_client(phone, email, name, sex):
+def add_client(phone, email, name, sex, rid):  # 增加旅客信息
     try:
         db = pymysql.connect(host='localhost', user='root',
                              password='yhv5tgh233', port=3306, db='datademo')
         cursor = db.cursor()
-        sql = "insert into main_client(phone, email, name, sex) values('%s','%s','%s','%s')" % (phone, email, name, sex)
+        sql = "insert into main_client(phone, email, name, sex, rid) values('%s','%s','%s','%s',%d)" % \
+              (phone, email, name, sex, rid)
         cursor.execute(sql)
         db.commit()
         return True
@@ -15,13 +16,14 @@ def add_client(phone, email, name, sex):
         return False
 
 
-def add_order(id, pay_method, state, submit_date, check_in_date, check_out_date):
+def add_order(id, phone, pay_method, state, submit_date, check_in_date, check_out_date):  # 增加订单信息
     try:
         db = pymysql.connect(host='localhost', user='root',
                              password='yhv5tgh233', port=3306, db='datademo')
         cursor = db.cursor()
-        sql = "insert into main_order(id, pay_method, state, submit_date, check_in_date, check_out_date) " \
-              "values('%s',%d,%d,'%s','%s','%s')" % (id, pay_method, state, submit_date, check_in_date, check_out_date)
+        sql = "insert into main_order(id, phone, pay_method, state, submit_date, check_in_date, check_out_date) " \
+              "values('%s','%s',%d,%d,'%s','%s','%s')" % (id, phone, pay_method, state, submit_date, check_in_date,
+                                                          check_out_date)
         cursor.execute(sql)
         db.commit()
         return True
@@ -29,7 +31,7 @@ def add_order(id, pay_method, state, submit_date, check_in_date, check_out_date)
         return False
 
 
-def add_room(id, kind, state):
+def add_room(id, kind, state):  # 增加房间信息
     try:
         db = pymysql.connect(host='localhost', user='root',
                              password='yhv5tgh233', port=3306, db='datademo')
@@ -40,4 +42,125 @@ def add_room(id, kind, state):
         return True
     except Exception as e:
         return False
+
+
+def seek_client(thephone):  # 按电话寻找旅客
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        sql = "select name,sex,phone,email,id,check_in_date,check_out_date from" \
+              "main_order left join main_client on main_order.phone = main_client.phone where phone='%d'" % thephone
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            name = row[0]
+            sex = row[1]
+            phone = row[2]
+            email = row[3]
+            id = row[4]
+            check_in_date = row[5]
+            check_out_date = row[6]
+            print(name, sex, phone, email, id, check_in_date, check_out_date)
+        db.close()
+    except:
+        print("Error:unable to fetch data")
+
+
+def seek_room():  # 查询房间余量
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        sql = "select kind,count(id) as num from" \
+              " main_room where state = 1 group by kind order by kind"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            kind = row[0]
+            num = row[1]
+            print(kind, num)
+        db.close()
+    except:
+        print("Error:unable to fetch data")
+
+
+def seek_order(theid):  # 查找订单
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        sql = "select phone,id,state,pay_method,submit_date,check_in_date,check_out_date from" \
+              " main_order where id='%d'" % theid
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            phone = row[0]
+            id = row[1]
+            state = row[2]
+            pay_method = row[3]
+            submit_date = row[4]
+            check_in_date = row[5]
+            check_out_date = row[6]
+            print(phone, id, state, pay_method, submit_date, check_in_date, check_out_date)
+        db.close()
+    except:
+        print("Error:unable to fetch data")
+
+
+def update_order_check(phone, new_check_in, new_check_out):  # 修改订单时间
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        now_time = datetime.datetime.now()
+        sql = "update main_order set check_in_date='%s',check_out_date='%s'" \
+              ",submit_date='%s' where phone='%s'" % (new_check_in, new_check_out, now_time, phone)
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except:
+        print("Error:unable to update date")
+
+
+def change_room_state(rid, state):  # 换房
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        sql = "update main_room set state = %d where id = %d" % (state, rid)
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except:
+        print("Error:unable to change room state")
+
+
+def change_room(phone, old_rid, new_rid):  # 换房
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        sql = "update main_client set rid = %d where phone='%s'" % (new_rid, phone)
+        cursor.execute(sql)
+        db.commit()
+        change_room_state(old_rid, 0)
+        change_room_state(new_rid, 1)
+        db.close()
+    except:
+        print("Error:unable to change rooms")
+
+
+def update_order_payment(phone):  # 订单付款
+    try:
+        db = pymysql.connect(host='localhost', user='root',
+                             password='yhv5tgh233', port=3306, db='datademo')
+        cursor = db.cursor()
+        now_time = datetime.datetime.now()
+        sql = "update main_order set state = 1 where phone='%s'" % phone
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except:
+        print("Error:unable to update pay state")
 
